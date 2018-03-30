beada3c2bec06cfd2dab259988ba49c4a24c6ab3
Alexandre <Santi.Alexandre@hotmail.fr>
2018-03-30 14:28:46 +0200

    Création des activity d'ajout
    Différentes activités de création de contenu créées (structure), et début de liens entre elles

---
+++ app/src/main/java/e/imh/nolifeagenda/AddPatron.java
@@ -0,0 +1,22 @@
+package e.imh.nolifeagenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alexandre on 30/03/2018.
+ */
+
+class AddPatron extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_addpatron);
+
+        //Récupération des informations
+        Intent intent = this.getIntent();
+    }
+}
--- app/src/main/java/e/imh/nolifeagenda/MainActivity.java
+++ app/src/main/java/e/imh/nolifeagenda/MainActivity.java
@@ -1,5 +1,6 @@
 package e.imh.nolifeagenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +15,53 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
-    public void onAjouterClic(View view)
+    public void onAjouterMenu(View view)
     {
-        //Ouvrir la liste de menus
+        if(this.findViewById(R.id.add_menu).getVisibility()==View.INVISIBLE)
+        {
+            this.findViewById(R.id.add_menu).setVisibility(View.VISIBLE);
+        }
+        else if(this.findViewById(R.id.add_menu).getVisibility()==View.VISIBLE)
+        {
+            this.findViewById(R.id.add_menu).setVisibility(View.INVISIBLE);
+        }
     }
+
+    public void onAjouterElement(View view)
+    {
+        Intent intent = null;
+
+        switch(view.getId())
+        {
+            case R.id.flux_rss_addBouton :
+                intent = new Intent(this, AddRSS.class);
+                break;
+
+            case R.id.event_addBouton :
+                intent = new Intent(this, AddEvent.class);
+                this.findViewById(R.id.add_menu).setVisibility(View.INVISIBLE);
+                break;
+
+            case R.id.rappel_addBouton :
+                intent = new Intent(this, AddRappel.class);
+                this.findViewById(R.id.add_menu).setVisibility(View.INVISIBLE);
+                break;
+
+            case R.id.liste_addBouton :
+                intent = new Intent(this, AddListe.class);
+                this.findViewById(R.id.add_menu).setVisibility(View.INVISIBLE);
+                break;
+
+            case R.id.patron_addBouton :
+                intent = new Intent(this,AddPatron.class);
+                this.findViewById(R.id.add_menu).setVisibility(View.INVISIBLE);
+                break;
+        }
+
+        if(intent!=null)
+        {
+            this.startActivity(intent);
+        }
+    }
+
 }
